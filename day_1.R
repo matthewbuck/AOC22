fc52fb4395572c87db002549a6e0dc2118e7da48
Matthew Buck <mbbuck@outlook.com>
2022-12-04 14:38:05 -0500

    Add files via upload

---
+++ day_1.R
@@ -0,0 +1,16 @@
+library('tidyverse')
+
+x = 'c:/users/mbbuc/documents/aoc22/Day 1/input.txt'
+df <- read_delim(x, delim = '\n', skip_empty_rows = FALSE, col_names = FALSE, col_types = 'd') %>%
+  mutate(group = cumsum(is.na(X1))) %>%
+  drop_na(X1) %>%
+  group_by(group) %>%
+  summarize(cals = sum(X1)) %>%
+  arrange(desc(cals))
+
+answer <- max(df$cals)
+
+# Part 2
+
+top_three <- head(df, 3)
+answer_two <- sum(top_three$cals)
